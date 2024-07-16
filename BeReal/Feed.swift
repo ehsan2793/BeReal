44b0527c08ac9e7e7e236b9896966ff41402fa8b
ehsan2793 <ehsan2793@gmail.com>
2024-07-15 23:14:18 -0700

    first commit

---
+++ BeReal/Feed.swift
@@ -0,0 +1,18 @@
+//
+//  Feed.swift
+//  BeReal
+//
+//  Created by Ehsan Rahimi on 7/15/24.
+//
+
+import SwiftUI
+
+struct Feed: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Feed()
+}
