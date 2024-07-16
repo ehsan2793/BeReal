6dc7fb59d24029a5cd9fcceb0a7466ae37e311db
ehsan2793 <ehsan2793@gmail.com>
2024-07-15 22:49:22 -0700

    Initial Commit

---
+++ BeReal/BeRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealApp.swift
+//  BeReal
+//
+//  Created by Ehsan Rahimi on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BeRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
